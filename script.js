1f63a6daf77e5a51eda044ef54cfcda351346314
Emma Paris <scimmia2002@gmail.com>
2022-08-07 23:52:33 +0200

    aggiunto movimento automatico, click tasti, frutto mangiato, aggiornamento coda, movimenti

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 var gridSize;
 let gridMatrix = [];
 let coords = [];
+let fruitCell = [];
+let lastDirection = [0, 0];
 
+let autoMoveTimer = null;
+let clickedWait = 250;
+let inGame = true;
 
 const placeHead = () => {
     coords = [];
     let y = randomInteger(gridSize);
     let x = randomInteger(gridSize);
-    coords.push(y);
-    coords.push(x);
+    coords.push([y, x]);
 
     gridMatrix[y][x] = 2;
-    console.log("head placed");
+    // console.log("head placed");
+}
+const addTail = () =>{
+    coords.push(coords[coords.length-1]);
+    console.log("new tail");
+    console.log(coords);
 }
 
 const placeFruit = () => {
+    fruitCell.pop();
+    fruitCell.pop();
+
     let y = randomInteger(gridSize);
     let x = randomInteger(gridSize);
 
@@ -22,7 +34,10 @@ const placeFruit = () => {
         y = randomInteger(gridSize);
         x = randomInteger(gridSize);
     }
-    
+
+    fruitCell.push(y);
+    fruitCell.push(x);
+
     gridMatrix[y][x] = 3;
     console.log("fruit placed");
 }
@@ -35,11 +50,11 @@ const newMatrix = () => {
     }
 
     for (let i = 0; i < gridSize; i++) {
-        for (let j = 0; j < gridSize; j++){
+        for (let j = 0; j < gridSize; j++)
             gridMatrix[i][j] = 0;
-        }
+        
     }
-    console.log(gridMatrix);
+    // console.log(gridMatrix);
 }
 
 
@@ -50,7 +65,7 @@ const setGridSize = () => {
 }
 
 const drawGrid = () => {
-    console.log("grid size: " + gridSize);
+    // console.log("grid size: " + gridSize);
     let grid = $("#grid");
 
     grid.empty();
@@ -66,17 +81,32 @@ const drawGrid = () => {
     }
     console.log("new grid");
 }
+const reloadDiv = (cells) => {
+    for(let i = 0; i < cells[length]; i++){
+        $(cells[i]).load(location.href + "#grid >" + cells[i]);
+    }
+}
 
+const checkEnd = () => {
+    console.log("check");
+    if(coords[0][0] < 0 || coords[0][0] < 0 || coords[0][0] > gridSize - 1 || coords[0][1] > gridSize - 1){
+        inGame = false;
+        // alert("you lost");
+        console.log("lost");
+    }
+}
 const drawContentes = () => {
+
+    let reloadCells = [];
     for (let i = 0; i < gridSize; i++) {
         for (let j = 0; j < gridSize; j++) {
             
             let cell = String(".cell_" + i + "_" + j); 
             let classes = ["head", "tail", "apple"];
+
             for(let k = 0; k < classes.length; k++){
                 if($(cell).hasClass(classes[k]))
                     $(cell).removeClass(classes[k]);
-                
             }
 
             // console.log(cell);
@@ -88,27 +118,14 @@ const drawContentes = () => {
             
             if(gridMatrix[i][j] == 3)
                 $(cell).addClass("apple");
-            
-            $(cell).load(location.href + "#grid >" + cell);
+
+            if(gridMatrix[i][j] != 0)
+                reloadCells.push(cell);
         }
     }
+    reloadDiv(reloadCells);
 
-    console.log("content drawed");
-}
-
-const checkNewCoords = () => {
-    if(coords[0] < 0 || coords[1] < 0)
-        playerCanMove = false;
-    else
-        for (let i = 0; i < gridSize; i++) {
-            for (let j = 0; j < gridSize; j++) {
-                if(gridMatrix[i][j] == 1 && i == coords[0] && j == coords[1])
-                    playerCanMove = false;
-                else if(i == coords[0] && j == coords[1])
-                    gridMatrix[i][j] = 2;
-            }
-        }
-    drawContentes();
+    // console.log("content drawed");
 }
 
 const gameInit = () => {
@@ -121,6 +138,68 @@ function randomInteger(max){
     return Math.floor(Math.random() * (max));
 }
 
+const updateGridValues = () =>{
+    for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
+            if(gridMatrix[i][j] != 3)
+                gridMatrix[i][j] = 0;
+        }
+    }
+    for (let i = 0; i < coords.length; i++) {
+        const tmp = coords[i];
+        // console.log(tmp);
+
+        if(i == 0)
+            gridMatrix[tmp[0]][tmp[1]] = 2;
+        else
+            gridMatrix[tmp[0]][tmp[1]] = 1;
+    }
+    // console.log(gridMatrix);
+}
+
+const fruitCollision = () => {
+    if(fruitCell[0] == coords[0][0] && fruitCell[1] == coords[0][1]){
+        $(".fruitNumder").html(parseInt($(".fruitNumder").html()) + 1);
+        placeFruit();
+        addTail();
+    }
+}
+
+const move = (direction) => {
+    // console.log(coords);
+    
+    coords.unshift(
+        [
+        coords[0][0] + direction[0]
+        ,
+        coords[0][1] + direction[1]
+        ]
+    );
+    coords.pop();
+
+    checkEnd();
+    // console.log(coords);
+    if(inGame){
+        fruitCollision();
+    
+        updateGridValues();
+    
+        
+        // console.log(coords);
+        drawContentes();
+    
+        lastDirection = direction;
+    
+        autoMove();
+    }
+    
+}
+
+const autoMove = () => {
+    autoMoveTimer = setTimeout(()=>{
+        move(lastDirection);
+    }, clickedWait);
+}
 
 $(document).ready(() => {
     setGridSize();
@@ -147,12 +226,15 @@ $(document).ready(() => {
     
     let playerCanMove = true;
 
-    $(document).keydown(function(event){
+    $(document).keydown((event) => {
 
         if(playerCanMove == true && (event.key == "ArrowUp" || event.key == "w")){
+            window.clearTimeout(autoMoveTimer); //cancel timeout
+            autoMoveTimer = null;
             $(".keyUp").css("border-color", "red").css("color", "red");
-            coords[0]--;
-            setTimeout(function (){
+            move([-1, 0]);
+            
+            setTimeout(() => {
                 $(".keyUp").css("border-color", "grey").css("color", "grey");
             }, 1000)
         }else{
@@ -160,20 +242,24 @@ $(document).ready(() => {
         }
 
         if(playerCanMove == true && (event.key == "ArrowRight" || event.key == "d")){
+            window.clearTimeout(autoMoveTimer); //cancel timeout
+            autoMoveTimer = null;
             $(".keyRight").css("border-color", "red").css("color", "red");
-            coords[1]++;
-            setTimeout(function (){
+            move([0, 1]);
+            
+            setTimeout(() => {
                 $(".keyRight").css("border-color", "grey").css("color", "grey");
             }, 1000)
         }else{
             $(".keyRight").css("border-color", "grey").css("color", "grey");
-
         }
 
         if(playerCanMove == true && (event.key == "ArrowDown" || event.key == "s")){
+            window.clearTimeout(autoMoveTimer); //cancel timeout
+            autoMoveTimer = null;
             $(".keyDown").css("border-color", "red").css("color", "red");
-            coords[0]++;
-            setTimeout(function (){
+            move([1, 0]);
+            setTimeout(() => {
                 $(".keyDown").css("border-color", "grey").css("color", "grey");
             }, 1000)
         }else{
@@ -181,14 +267,17 @@ $(document).ready(() => {
         }
 
         if(playerCanMove == true && (event.key == "ArrowLeft" || event.key == "a")){
+            window.clearTimeout(autoMoveTimer); //cancel timeout
+            autoMoveTimer = null;
             $(".keyLeft").css("border-color", "red").css("color", "red");
-            coords[1]--;
-            setTimeout(function (){
+            move([0, -1]);
+            setTimeout(() => {
             $(".keyLeft").css("border-color", "grey").css("color", "grey");
                 }, 1000)
         }else{
             $(".keyLeft").css("border-color", "grey").css("color", "grey");
         }
-        checkNewCoords();
+        
     });
+    
 });
